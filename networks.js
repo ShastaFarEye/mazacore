47a5e14f7c14c3e6fc71060150ad20bdeea84b96
Kefkius <kefkius@mail.com>
2014-09-08 12:54:17 -0400

    Mazacoin conversion update mainnet to mazacoin
    build as mazacore instead of bitcore
    
    change basic examples
    
    change getBlockValue for MZC
    
    update package.json
    
    change package.json to reflect new repo

--- networks.js
+++ networks.js
@@ -6,31 +6,28 @@ var hex = function(hex) {
 
 exports.livenet = {
   name: 'livenet',
-  magic: hex('f9beb4d9'),
-  addressVersion: 0x00,
-  privKeyVersion: 128,
+  magic: hex('f8b503df'),
+  addressVersion: 0x32,
+  privKeyVersion: 224,
   P2SHVersion: 5,
   hkeyPublicVersion: 0x0488b21e,
   hkeyPrivateVersion: 0x0488ade4,
   genesisBlock: {
-    hash: hex('6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000'),
-    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
+    hash: hex('0cc638e2c6b5b4b1446d36145627e30becc60f3fe1da784160ced8737c0c0000'),
+    merkle_root: hex('4F9F58D1D1C7DD6C2818DABB394AF876BB8D16FC4C59D99D9834588E3796D462'),
     height: 0,
-    nonce: 2083236893,
+    nonce: 2091390249,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1231006505,
-    bits: 486604799,
+    timestamp: 1390747675,
+    bits: 504365040,
   },
   dnsSeeds: [
-    'seed.bitcoin.sipa.be',
-    'dnsseed.bluematt.me',
-    'dnsseed.bitcoin.dashjr.org',
-    'seed.bitcoinstats.com',
-    'seed.bitnodes.io',
-    'bitseed.xf2.org'
+    'node.mazacoin.org',
+    'node.mazacoin.cf',
+    'mazacoin.no-ip.org'
   ],
-  defaultClientPort: 8333
+  defaultClientPort: 12835
 };
 
 exports.mainnet = exports.livenet;
